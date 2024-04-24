373be9c30809cb4e9760472c653d73d89750fbe0
Kat Inchoco <88949861+kpinchoco@users.noreply.github.com>
2024-04-24 11:04:01 -0400

    comment out pos embed print statement

--- src/models/vision_transformer.py
+++ src/models/vision_transformer.py
@@ -173,7 +173,7 @@ class VisionTransformer(nn.Module):
         if pos_embed is not None:
             x += pos_embed
         B, N, D = x.shape
-        print("pos_embed x shape:", x.shape)
+        # print("pos_embed x shape:", x.shape)
         # Mask away unwanted tokens (if masks provided)
         if masks is not None:
             x = apply_masks(x, masks)
@@ -191,7 +191,7 @@ class VisionTransformer(nn.Module):
 
         if self.norm is not None:
             x = self.norm(x)
-        print("shape of encoder output:", x.shape)
+        print("shape of encoder:", x.shape)
         return x
 
     def interpolate_pos_encoding(self, x, pos_embed):
