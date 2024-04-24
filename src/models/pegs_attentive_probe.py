712ff564444e2c0af3ee45deccbb1bf4840f8771
Kat Inchoco <88949861+kpinchoco@users.noreply.github.com>
2024-04-24 10:57:11 -0400

    sum across dim=1 instead of flatten

--- src/models/pegs_attentive_probe.py
+++ src/models/pegs_attentive_probe.py
@@ -13,16 +13,18 @@ class PegAttentiveClassifier(nn.Module):
     """ Attentive Classifier """
     def __init__(
         self,
-        embed_dim=768, #note that the embed dim gets set from the encoder parameters (vit)
+        embed_dim=768, # note that the embed dim gets set from the encoder parameters (vit)
         num_classes=165
     ):
         super().__init__()
         self.linear = nn.Linear(12544*embed_dim, num_classes, bias=False)
 
     def forward(self, x):
-        print("input to classifier x shape:", x.shape)
-        flattened_x = x.flatten(1,-1)
-        print("flattened x:", flattened_x.shape)
-        x = self.linear(flattened_x)
+        x = torch.sum(x, dim=1)
+        x = self.linear(x)
         return x
-
+        
+        # print("input to classifier x shape:", x.shape)
+        # flattened_x = x.flatten(1,-1)
+        # print("flattened x:", flattened_x.shape)
+        # x = self.linear(flattened_x)
