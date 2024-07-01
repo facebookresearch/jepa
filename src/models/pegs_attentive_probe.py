45d5e0b4988cb121469d84ca47bd50c63319fee6
Kat Inchoco <88949861+kpinchoco@users.noreply.github.com>
2024-06-30 22:07:56 -0400

    add avgpool(10, stride = 2)

--- src/models/pegs_attentive_probe.py
+++ src/models/pegs_attentive_probe.py
@@ -20,6 +20,7 @@ class PegAttentiveClassifier(nn.Module):
         # self.linear = nn.Linear(12544*embed_dim, num_classes, bias=False)
         self.linear = nn.Linear(embed_dim, num_classes, bias=True)
         self.softmax = nn.Softmax()
+        self.avgpool = nn.AvgPool2d(10, stride=2)
 
     def forward(self, x):
         print("input to classifier shape:", x.shape)
@@ -27,9 +28,12 @@ class PegAttentiveClassifier(nn.Module):
         print("summed x:", x.shape)
         print("min after sum", torch.min(x))
         print("max after sum", torch.max(x))
-        x = self.softmax(x)
-        print("min after softmax", torch.min(x))
-        print("max after softmax", torch.max(x))
+        # x = self.softmax(x)
+        # print("min after softmax", torch.min(x))
+        # print("max after softmax", torch.max(x))
+        x = self.avgpool(x)
+        print("min after avgpool", torch.min(x))
+        print("max after avgpool", torch.max(x))
         x = self.linear(x)
         print("min after linear", torch.min(x))
         print("max after linear", torch.max(x))
