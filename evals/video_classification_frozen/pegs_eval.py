753cf8401510f32900c7e3d917b4b22e13caf74e
Kat Inchoco <88949861+kpinchoco@users.noreply.github.com>
2024-08-01 14:44:15 -0400

    print sigmoid_outputs

--- evals/video_classification_frozen/pegs_eval.py
+++ evals/video_classification_frozen/pegs_eval.py
@@ -361,7 +361,8 @@ def run_one_epoch(
 
         sigmoid = torch.nn.Sigmoid()
         sigmoid_outputs = sigmoid(outputs[0][0,:])
-        print("sigmoud_outputs shape:", sigmoid_outputs.shape)
+        print("sigmoid outouts:", sigmoid_outputs)
+        print("sigmoid_outputs shape:", sigmoid_outputs.shape)
         plot_guess_img(sigmoid_outputs, output_filename = 'outputs-0.png')
         plot_guess_img(labels[0], output_filename = 'labels-0.png')
 
