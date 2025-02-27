facebookresearch/jepa

commit 787b04ae6c573be587d6afccea5eca6b9fde9039

Update modules.py

src/models/utils/modules.py
@@ -154,6 +154,8 @@ class CrossAttention(nn.Module):
             q = (xattn @ v)
 
         q = q.transpose(1, 2).reshape(B, n, C)
+        q = self.proj(q)
+    
         return q
 
 

commit a0517b4d6c7753605a763d75c0cb3310fb852221

REVERTING - commit 787b04a which broke evals

src/models/utils/modules.py
@@ -154,8 +154,6 @@ class CrossAttention(nn.Module):
             q = (xattn @ v)
 
         q = q.transpose(1, 2).reshape(B, n, C)
-        q = self.proj(q)
-    
         return q
 
 